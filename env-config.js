98397fa12c1f55d41812250b9099522c89ec1a28
tiliositar <tiliositar001@gmail.com>
2026-05-10 02:49:03 +0300

    Update env-config.js

--- env-config.js
+++ env-config.js
@@ -4,4 +4,4 @@
  */
 window.API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
   ? '' // Use relative paths for local development
-  : 'https://bu-alumni-portal-11.onrender.com'
+  : 'https://bu-alumni-portal-11.onrender.com';
